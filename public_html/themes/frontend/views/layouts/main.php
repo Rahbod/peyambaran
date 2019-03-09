1087b426ac46ee4eea46af43882cd8676bbc99d0
mustafa-rezae <mustafa.rezae01@gmail.com>
2019-03-09 19:31:31 +0330

    optimizing home page

--- public_html/themes/frontend/views/layouts/main.php
+++ public_html/themes/frontend/views/layouts/main.php
@@ -13,7 +13,7 @@ AppAsset::register($this);
 <!DOCTYPE html>
 <html lang="<?= Yii::$app->language; ?>">
 <head>
-    
+
     <meta charset="<?= Yii::$app->charset; ?>">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <?= Html::csrfMetaTags() ?>
