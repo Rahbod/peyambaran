4681675a327546d80ffc8fd7c05b6b9a57048ab5
mustafa-rezae <mustafa.rezae01@gmail.com>
2019-03-09 21:22:46 +0330

    optimizing home page

--- public_html/themes/frontend/js/jquery.script.js
+++ public_html/themes/frontend/js/jquery.script.js
@@ -43,44 +43,14 @@ $(function () {
             $('header').removeClass('shadow');
     });
 
+
     $("body").on("mouseover", "li.dropdown", function () {
         $(this).addClass("open");
     }).on("mouseleave", "li.dropdown", function () {
         $(this).removeClass("open");
     });
-
-    //small mode sidebar
-    $('#dismiss, .sidebar--overlay').on('click', function () {
-        // hide sidebar
-        $('#sidebar').removeClass('active');
-        // hide overlay
-        $('.sidebar--overlay').removeClass('active');
-    });
-
-    $('#sidebarCollapse').on('click', function () {
-        // open sidebar
-        $('#sidebar').addClass('active');
-        // fade in the overlay
-        $('.sidebar--overlay').addClass('active');
-        $('.collapse.in').toggleClass('in');
-        $('a[aria-expanded=true]').attr('aria-expanded', 'false');
-    });
-
-    //doctors script
-    $('.panel-title').on('click', function (event) {
-        $('.overlay').toggle();
-        var This = $(event.target);
-        var panel_parent = This.parents('.panel');
-        var el_id = This.parents('.panel').find('div.collapse').attr('id');
-        $('#' + el_id).collapse();
-        if (panel_parent.hasClass('-z-index')) {
-            panel_parent.removeClass('-z-index')
-        }
-        else {
-            panel_parent.addClass('-z-index')
-        }
-    })
-
+});
+(function () {
     /// define functions
     $.fn.niceScrollTrigger = function (refresh) {
         this.each(function () {
@@ -98,21 +68,54 @@ $(function () {
             nicescrolls.push({id: $(this).attr('id'), instance: instance});
         });
     };
-
-    // hide or show the main navbar base on page scroll : start
-    var position = $(window).scrollTop();
-    $(window).scroll(function () {
-        var scroll = $(window).scrollTop();
-        if (scroll > position) {
-            $('header').addClass('toggleMainNav');
-
-        } else {
-            $('header').removeClass('toggleMainNav');
-        }
-        position = scroll;
-    });
-
 })(jQuery);
 
+// hide or show the main navbar base on page scroll : start
+var position = $(window).scrollTop();
+$(window).scroll(function () {
+    var scroll = $(window).scrollTop();
+    if (scroll > position) {
+        $('header').addClass('toggleMainNav');
 
+    } else {
+        $('header').removeClass('toggleMainNav');
+    }
+    position = scroll;
+});
+// hide or show the main navbar base on page scroll : end
+
+$(document).ready(function () {
+//small mode sidebar
+    $('#dismiss, .sidebar--overlay').on('click', function () {
+        // hide sidebar
+        $('#sidebar').removeClass('active');
+        // hide overlay
+        $('.sidebar--overlay').removeClass('active');
+    });
+
+    $('#sidebarCollapse').on('click', function () {
+        // open sidebar
+        $('#sidebar').addClass('active');
+        // fade in the overlay
+        $('.sidebar--overlay').addClass('active');
+        $('.collapse.in').toggleClass('in');
+        $('a[aria-expanded=true]').attr('aria-expanded', 'false');
+    });
+
+//doctors script
+    $('.panel-title').on('click', function (event) {
+        $('.overlay').toggle();
+        var This = $(event.target);
+        var panel_parent = This.parents('.panel');
+        var el_id = This.parents('.panel').find('div.collapse').attr('id');
+        $('#' + el_id).collapse();
+        if (panel_parent.hasClass('-z-index')) {
+            panel_parent.removeClass('-z-index')
+        }
+        else {
+            panel_parent.addClass('-z-index')
+        }
+    })
+
+});
 
