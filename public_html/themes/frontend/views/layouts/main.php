0e8681a3351bbb5dfc43814986bfb095b12be673
mustafa-rezae <mustafa.rezae01@gmail.com>
2019-03-09 19:50:02 +0330

    optimizing home page

--- public_html/themes/frontend/views/layouts/main.php
+++ public_html/themes/frontend/views/layouts/main.php
@@ -25,6 +25,7 @@ AppAsset::register($this);
     <?php $this->registerCssFile($this->theme->baseUrl.'/css/owl.theme.default.min.css', ['depends' => [BootstrapAsset::className()],'media' => 'all'], 'owl-theme');?>
     <?php $this->registerCssFile($this->theme->baseUrl.'/css/bootstrap-theme.css', ['depends' => [BootstrapAsset::className()],'media' => 'all'], 'bootstrap-theme');?>
     <?php $this->registerCssFile($this->theme->baseUrl.'/css/responsive-theme.css', ['depends' => [BootstrapAsset::className()],'media' => 'all'], 'responsive-theme');?>
+    <?php $this->registerCssFile($this->theme->baseUrl.'/js/vendors/icomoon/style.css', ['depends' => [BootstrapAsset::className()],'media' => 'all'], 'icomoon');?>
     <?php $this->registerJsFile($this->theme->baseUrl.'/js/bootstrap.min.js', ['depends' => [JqueryAsset::className()]], 'bootstrap');?>
     <?php $this->registerJsFile($this->theme->baseUrl.'/js/jquery.nicescroll.min.js', ['depends' => [JqueryAsset::className()]], 'nicescroll');?>
     <?php $this->registerJsFile($this->theme->baseUrl.'/js/owl.carousel.min.js', ['depends' => [JqueryAsset::className()]], 'owl-script');?>
