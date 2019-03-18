de66d70edde6cef3b6040e12a58edb930d9cd75b
mustafa-rezae <mustafa.rezae01@gmail.com>
2019-03-18 13:50:11 +0330

    doctors content-header

--- private_html/views/person/_item_view.php
+++ private_html/views/person/_item_view.php
@@ -31,7 +31,7 @@
                         $rel = $day->getPersonsRel()->andWhere(['personID' => $model->id])->one();
                         ?>
                         <div class="panel-title__doctorTimes">
-                            <span class="icomoon-success"><?= jDateTime::date('l', $day->date) ?></span>
+                            <span><i class="icomoon-success ml-3" style="margin-bottom: -5px;"></i><?= jDateTime::date('l', $day->date) ?></span>
                             <span><?= Yii::t('words', 'show_time', [
                                     'start_time' => $rel->start_time,
                                     'end_time' => $rel->end_time,
