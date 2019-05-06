8f1a686e6e8f135caf6e36e49f193e31ca9643f8
YusefMobasheri <yusef.mobasheri@gmail.com>
2019-05-06 13:07:45 +0430

    Advice user side finish

---
+++ private_html/components/SmsSender.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: y_mobasheri
+ * Date: 5/6/2019
+ * Time: 12:21 PM
+ */
+
+namespace app\components;
+
+
+class SmsSender
+{
+    private $apiKey = '4C53506D4D337769526D425A4A6651657970446B6476424A754A687553304F67';
+
+
+}
